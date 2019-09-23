98590fda28498ebc120a965104fac3dc8cf01b29
su qinglin <1222>
2019-09-23 09:49:01 +0800

    获取token接口新增设备UUID和model
    (cherry picked from commit 1ed2118)

--- app/src/main/java/com/nexless/devicecollect/model/UploadDeviceInfoResponse.java
+++ app/src/main/java/com/nexless/devicecollect/model/UploadDeviceInfoResponse.java
@@ -14,6 +14,26 @@ public class UploadDeviceInfoResponse {
 
     private String token;
 
+    private String uuid;
+
+    private String model;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
     public String getToken() {
         return token;
     }
